ac9ac5da7a60a72c1222c2889d5b777c4eacaadc
dobidugi <dobidugi@gmail.com>
2023-02-04 02:17:44 +0900

    Design: 회원가입, 로그인 페이지에서 사용되는 SubmitButton Style 작성

---
+++ src/style/SignSubmitButtonStyle.ts
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+
+/**
+ * 회원가입, 로그인 페이지의 Submit 버튼 스타일
+ */
+const SignSubmitButtonStyle = css`
+    margin-top: 1rem;
+    cursor: pointer;
+    width: 100%;
+    text-align: center;
+    height: 48px;
+    background-color: var(--green-200);
+    border-radius: 4px;
+    color: var(--white);
+    &:hover {
+        background-color: var(--green-300);
+    }
+`
+export default SignSubmitButtonStyle;
