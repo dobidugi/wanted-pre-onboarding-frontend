b9de1f959a479219c0daf0c96f78276ca54ea639
dobidugi <dobidugi@gmail.com>
2023-02-04 01:15:19 +0900

    Docs: GreyBackgroundWrapper 주석 추가

--- src/components/common/GreyBackgroundWrapper.tsx
+++ src/components/common/GreyBackgroundWrapper.tsx
@@ -2,10 +2,16 @@ import { css } from '@emotion/react'
 import React from 'react'
 import BaseProps from '../../types/BaseProps';
 
+/**
+ * 회색 배경을 가지고 컴포넌트를 감싸는 컴포넌트
+ * @param props.children - 자식 컴포넌트
+ * @param props.className - 추가할 클래스 이름 (옵션)
+ * @returns 
+ */
 function GreyBackgroundWrapper(props: { children: React.ReactNode } & BaseProps) {
     return (
         <div
-            className={props?.className}
+            className={`${props?.className} grey-background-wrapper`}
             css={style}
         >
             {props.children}
@@ -14,7 +20,6 @@ function GreyBackgroundWrapper(props: { children: React.ReactNode } & BaseProps)
 }
 const style = css`
     width: 100%;
-    height: 100vh;
     background-color: var(--grey-100);
 `
 
