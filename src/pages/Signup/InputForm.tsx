5d08027b1faa3f9ce5b7a25982ae65aaca8e9bf3
dobidugi <dobidugi@gmail.com>
2023-02-04 12:38:50 +0900

    Refactor: 회원가입폼 리팩토링

--- src/pages/Signup/InputForm.tsx
+++ src/pages/Signup/InputForm.tsx
@@ -1,8 +1,16 @@
-import { css } from '@emotion/react'
-import React from 'react'
-import Button from '../../components/common/Button'
-import LabelInput from '../../components/common/LabelInput'
-import SignSubmitButtonStyle from '../../style/SignSubmitButtonStyle'
+import { css } from "@emotion/react";
+import React from "react";
+import Button from "../../components/common/Button";
+import LabelInput from "../../components/common/LabelInput";
+import { SignupContext } from "../../store/SignupContext";
+import SignSubmitButtonStyle from "../../style/SignSubmitButtonStyle";
+import BaseProps from "../../types/BaseProps";
+import InputProps from "../../types/InputProps";
+
+interface inputsType {
+    label: string;
+    input: InputProps & BaseProps
+}
 
 /**
  * 회원가입 입력 폼 컴포넌트
@@ -10,26 +18,41 @@ import SignSubmitButtonStyle from '../../style/SignSubmitButtonStyle'
  * @returns 
  */
 function InputForm() {
+    const { values, onChange, onSubmit } = React.useContext(SignupContext);
+    const inputs: inputsType[] = [
+        {
+            label: "이메일",
+            input: {
+                type: "email",
+                name: "email",
+                testId: "email-input",
+                value: values.email,
+                onChange: onChange,
+            }
+        },
+        {
+            label: "비밀번호",
+            input: {
+                type: "password",
+                name: "password",
+                testId: "password-input",
+                value: values.password,
+                onChange: onChange,
+            }
+        },
+    ]
     return (
         <form
+            onSubmit={onSubmit}
             css={style}
         >
-            <LabelInput
-                label="이메일"
-                input={{
-                    type: "email",
-                    name: "email",
-                    testId: "email-input"
-                }}
-            />
-            <LabelInput
-                label="비밀번호"
-                input={{
-                    type: "password",
-                    name: "password",
-                    testId: "password-input"
-                }}
-            />
+            {inputs.map((input, index) => (
+                <LabelInput
+                    key={index}
+                    label={input.label}
+                    input={input.input}
+                />
+            ))}
             <Button
                 type="submit"
                 testId="signup-button"
@@ -38,11 +61,11 @@ function InputForm() {
                 회원 가입
             </Button>
         </form>
-    )
+    );
 }
 
 const style = css`
     width: 100%;
-`
+`;
 
 export default InputForm;
