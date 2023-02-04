64ad39008974616f063aff530ecba6d780bac3a6
dobidugi <dobidugi@gmail.com>
2023-02-04 16:34:34 +0900

    Feat: /todo 페이지 라우터에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./style/GlobalStyle";
+import { css } from "@emotion/react";
 
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-import { css } from "@emotion/react";
+import Todo from "./pages/todo";
+
+
 
 function App() {
     return (
@@ -14,6 +17,7 @@ function App() {
                 <Routes>
                     <Route path="/signin" element={<Signin />} />
                     <Route path="/signup" element={<Signup />} />
+                    <Route path="/todo" element={<Todo />} />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ src/pages/todo/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * /todo
+ * todo 컴포넌트
+ * @returns 
+ */
+function Index() {
+    return (
+        <div>todo</div>
+    )
+}
+
+export default Index;
