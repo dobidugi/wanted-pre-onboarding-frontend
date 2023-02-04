00e22dde4cdfd3d654a86872200a90780248975e
dobidugi <dobidugi@gmail.com>
2023-02-04 17:44:26 +0900

    Feat: Todo 타입 정의

---
+++ src/types/Todo.ts
@@ -0,0 +1,16 @@
+/**
+ * Todo 타입
+ * @typedef {Object} Todo
+ * @property {number} id - Todo id
+ * @property {string} todo - Todo 내용
+ * @property {boolean} isCompleted - Todo 완료 여부
+ * @property {number} userId - Todo를 생성한 유저 id
+ */
+interface Todo {
+    id: 1,
+    todo: string,
+    isCompleted: false,
+    userId: number
+}
+
+export default Todo
