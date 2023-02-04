5973c9c30fddf00c47db4bcbd7bfb2fc89fa64cc
dobidugi <dobidugi@gmail.com>
2023-02-04 20:33:37 +0900

    Design: Completed Todo 일시 todo text 밑줄처리

--- src/pages/todo/TodoItem.tsx
+++ src/pages/todo/TodoItem.tsx
@@ -23,7 +23,10 @@ function TodoItem(props: Todo) {
                     defaultChecked={props.isCompleted}
                     onClick={() => onClickCheckBox(props.id)}
                 />
-                <span>{props.todo}</span>
+                <span
+                    className={`${props.isCompleted ? "checked" : ""}`}>
+                    {props.todo}
+                </span>
             </label>
             <Button
                 type="button"
@@ -58,6 +61,10 @@ const style = css`
         span {
             font-size: 1rem;
             margin-left: 0.5rem;
+        }
+        
+        span.checked {
+            text-decoration: line-through;
         }
 
         input {
