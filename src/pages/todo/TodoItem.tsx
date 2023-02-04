fa8dbe1557d9ef28312f655e34de54d1b74608a1
dobidugi <dobidugi@gmail.com>
2023-02-04 20:59:20 +0900

    Feat: 특정 Todo 수정 모드로 전환후 수정하는 기능

--- src/pages/todo/TodoItem.tsx
+++ src/pages/todo/TodoItem.tsx
@@ -1,13 +1,20 @@
 import { css } from '@emotion/react';
-import React, { useCallback, useContext } from 'react'
+import React, { useCallback, useContext, useState } from 'react'
 import Todo from '../../types/Todo';
 import Button from '../../components/common/Button';
 import TodoContext from '../../store/TodoContext';
+import Input from '../../components/common/Input';
 
 function TodoItem(props: Todo) {
     const { todos } = useContext(TodoContext);
     const { onDelete, onUpdate } = todos;
-    // const [isEditMode, setIsEditMode] = useState<boolean>(false);
+    const [todo, setTodo] = useState<string>(props.todo);
+    const [isEditMode, setIsEditMode] = useState<boolean>(false);
+
+    const onChangeTodo = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        setTodo(e.target.value);
+    }, [])
+
     const onClickCheckBox = useCallback((id: number) => {
         const newTodo: Todo = {
             ...props,
@@ -15,32 +22,91 @@ function TodoItem(props: Todo) {
         }
         onUpdate(newTodo);
     }, [onUpdate, props]);
+
+    const onModifyTodo = useCallback(() => {
+        const newTodo: Todo = {
+            ...props,
+            todo: todo
+        }
+        onUpdate(newTodo);
+        setIsEditMode(false);
+    }, [onUpdate, props, todo])
+
+    const onChangeEditModeState = useCallback((state: boolean) => {
+        if (state === true) {
+            setTodo(props.todo);
+        }
+        setIsEditMode(() => state);
+    }, [props.todo]);
+
     return (
         <li css={style}>
-            <label>
-                <input
-                    type="checkbox"
-                    defaultChecked={props.isCompleted}
-                    onClick={() => onClickCheckBox(props.id)}
-                />
-                <span
-                    className={`${props.isCompleted ? "checked" : ""}`}>
-                    {props.todo}
-                </span>
-            </label>
-            <Button
-                type="button"
-                testId="modify-button"
-            >
-                수정
-            </Button>
-            <Button
-                type="button"
-                testId="delete-button"
-                onClick={() => onDelete(props.id)}
-            >
-                삭제
-            </Button>
+            {
+                isEditMode ?
+                    <React.Fragment>
+                        <input
+                            type="checkbox"
+                            defaultChecked={props.isCompleted}
+                            onClick={() => onClickCheckBox(props.id)}
+                        />
+                        <Input
+                            type="text"
+                            name="editTodo"
+                            testId="modify-input"
+                            value={todo}
+                            onChange={onChangeTodo}
+                        />
+                    </React.Fragment>
+                    :
+                    <label>
+                        <input
+                            type="checkbox"
+                            defaultChecked={props.isCompleted}
+                            onClick={() => onClickCheckBox(props.id)}
+                        />
+                        <span
+                            className={`${props.isCompleted ? "checked" : ""}`}>
+                            {props.todo}
+                        </span>
+                    </label>
+            }
+
+            {
+                isEditMode ?
+                    <React.Fragment>
+                        <Button
+                            type="button"
+                            testId="submit-button"
+                            onClick={onModifyTodo}
+                        >
+                            제출
+                        </Button>
+                        <Button
+                            type="button"
+                            testId="cancel-button"
+                            onClick={() => onChangeEditModeState(false)}
+                        >
+                            취소
+                        </Button>
+                    </React.Fragment>
+                    :
+                    <React.Fragment>
+                        <Button
+                            type="button"
+                            testId="modify-button"
+                            onClick={() => onChangeEditModeState(true)}
+                        >
+                            수정
+                        </Button>
+                        <Button
+                            type="button"
+                            testId="delete-button"
+                            onClick={() => onDelete(props.id)}
+                        >
+                            삭제
+                        </Button>
+                    </React.Fragment>
+            }
         </li>
     )
 }
@@ -86,12 +152,20 @@ const style = css`
         background-color: var(--blue-300);
     }
 
-    button[data-testid="delete-button"] {
+    button[data-testid="delete-button"], button[data-testid="cancel-button"] {
         background-color: var(--coral);
     }
 
-    button[data-testid="delete-button"]:hover {
+    button[data-testid="delete-button"]:hover, button[data-testid="cancel-button"]:hover {
         background-color: var(--red-400);
+    }
+
+    button[data-testid="submit-button"] {
+        background-color: var(--green-200);
+    }
+
+    button[data-testid="submit-button"]:hover {
+        background-color: var(--green-300);
     }
 
     
